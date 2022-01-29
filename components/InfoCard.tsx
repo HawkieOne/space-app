4db35cd3884b244df3dbb9e975a4b2bb62248582
Håkan Lindahl <59147469+HawkieOne@users.noreply.github.com>
2022-01-30 00:34:24 +0100

    added component for various info

---
+++ components/InfoCard.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function InfoCard() {
+  return (
+    <div className="info-card">
+        <div className="pl-2 pt-2">
+            Hello
+        </div>
+        <div className="flex flex-col justify-center items-center divide-y py-2">
+            <p>8888</p>
+            <p>Past day</p>
+        </div>
+    </div>
+  );
+}
