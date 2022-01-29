985cc9ee6ee6b8b385fa753f56c675498033e0a4
Håkan Lindahl <59147469+HawkieOne@users.noreply.github.com>
2022-01-30 00:34:06 +0100

    added placeholder header

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div className="text-center bg-slate-800 text-white p-6">
+        <h1>Space Wisdom</h1>
+    </div>
+  );
+}
