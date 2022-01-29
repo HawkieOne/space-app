33d1f1499a9fb470fe85bc7f8a16cef439357575
Håkan Lindahl <59147469+HawkieOne@users.noreply.github.com>
2022-01-30 00:33:43 +0100

    added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'spacepurple': '1A1A40',
+        'spaceblue': '270082',
+        'spacedarkpink': '7A0BC0',
+        'spacepink': 'FA58B6'
+      },
+    },
   },
   plugins: [],
 }
