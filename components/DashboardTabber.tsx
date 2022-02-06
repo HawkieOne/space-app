f97157e39e10c3237fd7bde7c6d2d8a594143ce2
Håkan Lindahl <59147469+HawkieOne@users.noreply.github.com>
2022-02-06 19:44:05 +0100

    loops through navLinks

--- components/DashboardTabber.tsx
+++ components/DashboardTabber.tsx
@@ -1,34 +1,23 @@
 import React from 'react';
+import Link from 'next/link'
+import { navLinks } from '.././shared/data';
 
 export default function DashboardTabber() {
   return (
     <div className="">
-        <nav className="bg-gray-800 rounded-2xl">
+        <nav className="bg-slate-800">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-center h-16">
                     <div className="flex items-center">
                         <div className="hidden md:block">
                             <div className="ml-10 flex items-baseline space-x-4">
-                                {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
-                                <a href="#" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Launches</a>
-
-                                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Events</a>
-
-                                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Agencies</a>
-
-                                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Astronauts</a>
-
-                                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Space stations</a>
-
-                                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Expeditions</a>
-
-                                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Dockings</a>
-
-                                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Launch vehicles</a>
-
-                                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Spacecraft</a>
-                                
-                                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Locations</a>
+                                {navLinks.map((item, index) => { 
+                                    return (
+                                        <Link href={item.path} key={index}>
+                                            <h5 className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer">{item.name}</h5>
+                                        </Link>
+                                    )
+                                })}
                             </div>
                         </div>
                     </div>
