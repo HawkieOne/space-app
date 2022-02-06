079dee078b7e52ff4a71432fe9294509e9d2d4f7
Håkan Lindahl <59147469+HawkieOne@users.noreply.github.com>
2022-02-06 19:45:24 +0100

    exports navlinks

---
+++ shared/data.ts
@@ -0,0 +1,44 @@
+export const navLinks = [
+    { name: "Home", 
+     path: "/" 
+    },
+    { name: "Launches", 
+     path: "Launches" 
+    },
+    {
+      name: "Events",
+      path: "/Events",
+    },
+    {
+      name: "Agencies",
+      path: "/Agencies",
+    },
+    {
+      name: "Astronauts",
+      path: "/Astronauts",
+    },
+    {
+      name: "Spacestations",
+      path: "/Spacestations",
+    },
+    {
+      name: "Expeditions",
+      path: "/Expeditions",
+    },
+    {
+        name: "Dockings",
+        path: "/Dockings",
+    },
+    {
+        name: "Launchvehicles",
+        path: "/Launchvehicles",
+    },
+    {
+        name: "Spacecraft",
+        path: "/Spacecraft",
+    },
+    {
+        name: "Locations",
+        path: "/Locations",
+    },
+  ];
