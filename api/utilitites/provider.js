15694ce7f735e6621359180ab0a6c07c0900076e
Håkan <hali0151@student.umu.se>
2022-03-03 09:41:21 +0100

    Added api call for astronauts
    Via the provider one can now get data
    about astronauts by calling apiCore.getAstronauts()

---
+++ api/utilitites/provider.js
@@ -0,0 +1,41 @@
+import axios from 'axios'; 
+import { handleResponse, handleError } from './response'; 
+
+const BASE_URL = 'https://lldev.thespacedevs.com/2.2.0/';
+
+const getLaunches = () => {
+    return axios
+        .get(`${BASE_URL}launch/upcoming`)
+        .then(handleResponse) 
+        .catch(handleError); 
+}
+
+const getEvents = () => {
+    return axios
+        .get(`${BASE_URL}event/upcoming`)
+        .then(handleResponse) 
+        .catch(handleError); 
+  };
+
+const getAgencies = () => {
+    return axios
+        .get(`${BASE_URL}agencies`)
+        .then(handleResponse) 
+        .catch(handleError); 
+};
+
+const getAstronauts = () => {
+    return axios
+        .get(`${BASE_URL}astronaut`)
+        .then(handleResponse) 
+        .catch(handleError); 
+};
+
+const apiProvider = {
+    getLaunches,
+    getEvents,
+    getAgencies,
+    getAstronauts
+}
+
+export default apiProvider;
