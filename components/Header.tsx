3f20994da14f28836de98498d3f53277dd231d82
Håkan <hali0151@student.umu.se>
2022-05-30 01:03:26 +0200

    new header
    - added headr image and better layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <div className="text-center bg-slate-800 text-white p-6">
-        <h1>Space Wisdom</h1>
+    <div className="flex justify-center items-center space-x-5 bg-slate-800 text-white p-6">
+          <div className="h-12 w-12 text-red-400">
+            <img src="space.svg" className="text-red-400" alt="space"></img>
+          </div>
+        <h1 className="text-3xl text-teal-600">Spac<span className="text-teal-500">Q</span></h1>
     </div>
   );
 }
