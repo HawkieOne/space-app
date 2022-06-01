fe6e0f3c568788acb57f673a67994ffe9ce08cbe
Restyled.io <commits@restyled.io>
2022-06-01 17:13:39 +0000

    Restyled by clang-format

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-  content: [
+  content : [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
-  theme: {
-    extend: {
-      colors: {
-        'spacepurple': '#1A1A40',
-        'spaceblue': '#270082',
-        'spacedarkpink': '#7A0BC0',
-        'spacepink': '#FA58B6',
-        'skeletonLight': '#d1d5db',
-        'skeletonDark': '#9ca3af',
+  theme : {
+    extend : {
+      colors : {
+        'spacepurple' : '#1A1A40',
+        'spaceblue' : '#270082',
+        'spacedarkpink' : '#7A0BC0',
+        'spacepink' : '#FA58B6',
+        'skeletonLight' : '#d1d5db',
+        'skeletonDark' : '#9ca3af',
       },
     },
   },
-  plugins: [
+  plugins : [
     require('tailwind-scrollbar'),
   ],
 }
