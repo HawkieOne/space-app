a3fc71abe5f7fee605e92f87d1cba57bd5fbd783
Håkan Lindahl <59147469+HawkieOne@users.noreply.github.com>
2022-06-01 19:10:20 +0200

    add colors
    - added dark gray fo skeleton content
    - added light grey for skeleton background

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'spacepurple': '1A1A40',
-        'spaceblue': '270082',
-        'spacedarkpink': '7A0BC0',
-        'spacepink': 'FA58B6'
+        'spacepurple': '#1A1A40',
+        'spaceblue': '#270082',
+        'spacedarkpink': '#7A0BC0',
+        'spacepink': '#FA58B6',
+        'skeletonLight': '#d1d5db',
+        'skeletonDark': '#9ca3af',
       },
     },
   },
