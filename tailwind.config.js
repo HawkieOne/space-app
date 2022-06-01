16a28a9f4ee8d9f13c88bc8a80d89c72eb755684
Restyled.io <commits@restyled.io>
2022-06-01 17:13:40 +0000

    Restyled by prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,19 @@
 module.exports = {
-  content : [
+  content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
-  theme : {
-    extend : {
-      colors : {
-        'spacepurple' : '#1A1A40',
-        'spaceblue' : '#270082',
-        'spacedarkpink' : '#7A0BC0',
-        'spacepink' : '#FA58B6',
-        'skeletonLight' : '#d1d5db',
-        'skeletonDark' : '#9ca3af',
+  theme: {
+    extend: {
+      colors: {
+        spacepurple: '#1A1A40',
+        spaceblue: '#270082',
+        spacedarkpink: '#7A0BC0',
+        spacepink: '#FA58B6',
+        skeletonLight: '#d1d5db',
+        skeletonDark: '#9ca3af',
       },
     },
   },
-  plugins : [
-    require('tailwind-scrollbar'),
-  ],
+  plugins: [require('tailwind-scrollbar')],
 }
