a114fa236eaafb29312993eb914d86a8d8817340
Håkan <hali0151@student.umu.se>
2022-09-20 19:36:03 +0200

    optimize and restructure

---
+++ components/shared/InfoColorPill.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type InfoColorPillProps = {
+    data: any
+    bgColor: string
+}
+
+export default function InfoColorPill({ data, bgColor } : InfoColorPillProps) {
+  return (
+    <div>
+      {data && (
+        <div className={`desktop:text-md flex items-center rounded-full ${bgColor} px-2 py-1 text-sm`}>
+          {data}
+        </div>
+      )}
+    </div>
+  )
+}
