30a2d1f270215fd61f923d3262e732bfe58b44ed
Ludvig Lindahl <ludde.lindahl@gmail.com>
2023-01-30 23:32:23 +0100

    add wikipedia button to spacestations

--- components/Spacestation/SpacestationCard.tsx
+++ components/Spacestation/SpacestationCard.tsx
@@ -1,7 +1,8 @@
 import { motion, AnimatePresence } from 'framer-motion'
 import React from 'react'
+import WikipediaButton from '../shared/WikipediaButton'
 
-export default function SpacestationCard({ spacestation, index} : any) {
+export default function SpacestationCard({ spacestation, index }: any) {
   return (
     <AnimatePresence>
       <motion.div
@@ -9,13 +10,13 @@ export default function SpacestationCard({ spacestation, index} : any) {
         animate={{ opacity: 1 }}
         initial={{ opacity: 0 }}
         exit={{ opacity: 0 }}
-        className="cursor-pointer overflow-hidden rounded bg-slate-800 shadow-lg hover:scale-105 hover:bg-spaceTealHover"
+        className="overflow-hidden rounded bg-slate-800 shadow-lg"
         key={spacestation.id}
       >
         <img
           className="max-h-52 w-full bg-white"
           src={spacestation.image_url}
-          alt="River"
+          alt="Space station"
         />
         <div className="px-4 py-2">
           <div className="mb-2 text-lg font-bold text-white">
@@ -25,16 +26,20 @@ export default function SpacestationCard({ spacestation, index} : any) {
             {/* {spacestation.description} */}
           </p>
         </div>
-        <div className="px-4 pt-2 pb-2">
-          {spacestation.deorbited !== null ? (
-            <span className="mr-2 mb-2 inline-block rounded-full bg-gray-900 px-3 py-1 text-sm font-semibold text-red-700">
+        <div className="flex justify-between px-4 py-2">
+          <span className="mr-2 mb- inline-block rounded-full bg-gray-900 px-3 py-1 text-sm font-semibold text-red-700">
+          { spacestation.deorbited !== null ? "Deorbited" : "In orbit" }
+          </span>
+          {/* {spacestation.deorbited !== null ? (
+            <span className="mr-2 mb- inline-block rounded-full bg-gray-900 px-3 py-1 text-sm font-semibold text-red-700">
               Deorbited
             </span>
           ) : (
             <span className="mr-2 mb-2 inline-block rounded-full bg-gray-900 px-3 py-1 text-sm font-semibold text-green-700">
               In orbit
             </span>
-          )}
+          )} */}
+          <WikipediaButton name={spacestation.name} />
         </div>
       </motion.div>
     </AnimatePresence>
