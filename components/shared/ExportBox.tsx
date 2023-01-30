570106f99013d0f3681da95959305c4b689e9920
Ludvig Lindahl <ludde.lindahl@gmail.com>
2023-01-30 22:38:44 +0100

    add wikipedia button for expeditions

--- components/shared/ExportBox.tsx
+++ components/shared/ExportBox.tsx
@@ -7,13 +7,15 @@ import ShareButton from './ShareButton'
 
 type ExportBoxProps = {
   fileName: string
+  wikipedia: boolean
   data: DownloadObject
 }
-export default function ExportBox({ fileName, data }: ExportBoxProps) {
+export default function ExportBox({ fileName, data, wikipedia }: ExportBoxProps) {
+  
   return (
     <div className="flex justify-start space-x-4">
       <DownloadButton data={data.content} fileName={fileName} fileType="json" />
-      <WikipediaButton name={fileName} />
+      { wikipedia && <WikipediaButton name={fileName} /> }
       <ShareButton data={data.content}>
         <div>
           <MenuCard onClick={() => {}}>Messenger</MenuCard>
